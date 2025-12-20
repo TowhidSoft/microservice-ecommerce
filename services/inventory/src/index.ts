2707dcdbe155d58b7c4f07d88653d3f3f451f6b0
TowhidSoft <devtowhid486@gmail.com>
2025-12-20 15:04:12 +0600

    Some fix:

--- services/inventory/src/index.ts
+++ services/inventory/src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello World!")
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import morgan from 'morgan'
+
+dotenv.config();
+
+const app = express()
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+
+app.get('/health', (_req, res) => {
+    res.status(200).json({ status: 'UP' })
+})
+
+
+// Error handler
+app.use((err, _req, res, _next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal server error' });
+})
+
+const port = process.env.PORT || 4002;
+const serviceName = process.env.SERVICE_NAME || 'inventory-service';
+
+app.listen(port, () => {
+    console.log(`${serviceName} us running on port ${port}`);
+
+})
