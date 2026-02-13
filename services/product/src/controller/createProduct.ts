7f17cf2c1b2ec0f4ec615141dd84597c2f7186c3
TowhidSoft <devtowhid486@gmail.com>
2026-02-14 02:42:38 +0600

    hush

--- services/product/src/controller/createProduct.ts
+++ services/product/src/controller/createProduct.ts
@@ -1,37 +1,63 @@
 import { ProductCreateDTOSchema } from "@/schema";
 import prisma from "../prisma";
 import { Request, Response, NextFunction } from "express";
+import axios from "axios";
+import { INVENTORY_URL } from "@/config";
 
-const createProduct = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const parsedBody = ProductCreateDTOSchema.safeParse(req.body);
-        if (!parsedBody.success) {
-            return res.status(400).json({ error: parsedBody.error })
-        }
-
-        // Create Inventory
-        const inventory = await prisma.inventory.create({
-            data: {
-                ...parsedBody.data,
-                histories: {
-                    create: {
-                        actionType: 'IN',
-                        quantityChanged: parsedBody.data.quantity,
-                        lastQuantity: 0,
-                        newQuantity: parsedBody.data.quantity,
-                    }
-                }
-            },
-            select: {
-                id: true,
-                quantity: true
-            }
-        });
-        return res.status(201).json(inventory);
-
-    } catch (error) {
-        next(error);
+const createProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    // Validate request body
+    const parsedBody = ProductCreateDTOSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      return res.status(400).json({ error: parsedBody.error });
     }
-}
 
-export default createInventory;
+    const existingProduct = await prisma.product.findFirst({
+      where: {
+        sku: parsedBody.data.sku,
+      },
+    });
+
+    if (existingProduct) {
+      return res
+        .status(400)
+        .json({ message: "Product with the same SKU already exists" });
+    }
+
+    // Create Inventory
+    const product = await prisma.product.create({
+      data: parsedBody.data,
+    });
+    console.log("Product created", product.id);
+
+    // Create inventory record for the product
+    const { data: inventory } = await axios.post(
+      `${INVENTORY_URL}/inventories`,
+      {
+        productId: product.id,
+        sku: product.sku,
+      },
+    );
+
+    console.log("Inventory created successfully", inventory.id);
+
+    // update product and store inventory id
+    await prisma.product.update({
+      where: { id: product.id },
+      data: {
+        inventoryId: inventory.id,
+      },
+    });
+    console.log("Product updated successfully with inventory");
+
+    return res.status(201).json({ ...product, inventoryId: inventory.id });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default createProduct;
