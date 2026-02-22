3e4f9e8185498f6f23c03e80d4b81ba38d7b2333
TowhidSoft <devtowhid486@gmail.com>
2026-02-22 16:02:44 +0600

    some update on part 2

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -2,22 +2,49 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import morgan from 'morgan'
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import { configureRoutes } from './util';
 
 dotenv.config();
 
 const app = express()
-app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
+
+// security middleware
+app.use(helmet());
+
+// Rate limiting middleware
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    handler: (req, res) => {
+        res.status(429).json({message: "Too many requests, please try again later."})
+    }
+})
+app.use('/api', limiter)
+
+
+// request logger 
+app.use(morgan('dev'))
+app.use(express.json())
+
+// Auth middleware
+
+configureRoutes(app)
 
 
 // 404 handler
-app.use((_req, res) => {
-    res.status(404).json({message: 'Route not found'})
+app.use((err,_req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({message: 'Internal Server Error'})
 })
 
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`API Gateway is running on port ${PORT}`);
+})
 
-app.listen(port, () => {
-    console.log(`${serviceName} us running on port ${port}`);
-
+// health check
+app.get('/health', (_req, res) => {
+    res.json({message: 'API Gateway is running'})
 })
